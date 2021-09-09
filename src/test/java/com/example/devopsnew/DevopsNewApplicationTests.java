6e95ed9e3ffab0748aa91ae2fabed9dc95c17dd9
Akash Dutta <Akash.Dutta2@mindtree.com>
2021-09-09 18:46:28 +0530

    Files added

---
+++ src/test/java/com/example/devopsnew/DevopsNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopsnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
